dfc206fee7641d1061391ce882f251d1f7639cec
chenpian <1837890880@qq.com>
2016-09-25 18:32:24 +0800

    Update Test.java

--- springmvc/src/main/java/com/springmvc/test/Test.java
+++ springmvc/src/main/java/com/springmvc/test/Test.java
@@ -3,7 +3,8 @@ package com.springmvc.test;
 public class Test {
 
 	public static void main(String[] args) {
-		System.out.println("1234");
+		System.out.println("12345");
+		System.out.println("1");
 	}
 
 }
