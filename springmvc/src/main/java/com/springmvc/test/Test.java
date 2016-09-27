b39238716271b0732132f4c07a595869fef959f1
chenpian <1837890880@qq.com>
2016-09-27 16:12:51 +0800

    initz

--- springmvc/src/main/java/com/springmvc/test/Test.java
+++ springmvc/src/main/java/com/springmvc/test/Test.java
@@ -3,7 +3,8 @@ package com.springmvc.test;
 public class Test {
 
 	public static void main(String[] args) {
-		System.out.println("12345678");
+		System.out.println("12345679");
+		
 		System.out.println("2");
 	}
 
