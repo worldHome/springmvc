4c8c12bceb80856084a3f0e82508d99dffaa1692
chenpian <1837890880@qq.com>
2016-09-27 15:51:26 +0800

    Update Test.java

--- springmvc/src/main/java/com/springmvc/test/Test.java
+++ springmvc/src/main/java/com/springmvc/test/Test.java
@@ -3,7 +3,7 @@ package com.springmvc.test;
 public class Test {
 
 	public static void main(String[] args) {
-		System.out.println("1234567");
+		System.out.println("12345678");
 		System.out.println("2");
 	}
 
