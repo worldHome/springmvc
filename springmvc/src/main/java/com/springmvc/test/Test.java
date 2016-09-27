e1f57d271832e07b97abada4fbc22985d4d1ebce
chenpian <1837890880@qq.com>
2016-09-27 17:17:59 +0800

    ddd

--- springmvc/src/main/java/com/springmvc/test/Test.java
+++ springmvc/src/main/java/com/springmvc/test/Test.java
@@ -3,9 +3,7 @@ package com.springmvc.test;
 public class Test {
 
 	public static void main(String[] args) {
-		System.out.println("12345679");
-		
-		System.out.println("2");
+		System.out.println("1234");
 	}
 
 }
