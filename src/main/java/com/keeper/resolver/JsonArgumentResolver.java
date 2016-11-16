2ff66ea97312f76d88da664056db726a938a77e4
chenpian <1837890880@qq.com>
2016-11-16 17:40:38 +0800

    jinit

--- src/main/java/com/keeper/resolver/JsonArgumentResolver.java
+++ src/main/java/com/keeper/resolver/JsonArgumentResolver.java
@@ -1,7 +1,10 @@
 package com.keeper.resolver;
 
+import java.io.IOException;
+
 import javax.servlet.http.HttpServletRequest;
 
+import org.apache.commons.io.IOUtils;
 import org.springframework.core.MethodParameter;
 import org.springframework.web.bind.support.WebDataBinderFactory;
 import org.springframework.web.context.request.NativeWebRequest;
@@ -9,21 +12,37 @@ import org.springframework.web.method.support.HandlerMethodArgumentResolver;
 import org.springframework.web.method.support.ModelAndViewContainer;
 
 public class JsonArgumentResolver implements HandlerMethodArgumentResolver {
+	private static final String REQUEST_BODY_ATTR = "REQUEST_BODY_ATTR";
+	private static final String ENCODING = "UTF-8";
 
 	@Override
-	public Object resolveArgument(MethodParameter parameter, ModelAndViewContainer mvcContainer, NativeWebRequest webRequest,
+	public Object resolveArgument(MethodParameter parameter,
+			ModelAndViewContainer mvcContainer, NativeWebRequest webRequest,
 			WebDataBinderFactory binderFactory) throws Exception {
-		HttpServletRequest request = webRequest.getNativeRequest(HttpServletRequest.class);
-		if(!request.getContentType().contains("json")){
+		HttpServletRequest request = webRequest
+				.getNativeRequest(HttpServletRequest.class);
+		if (!request.getContentType().contains("json")) {
 			return null;
 		}
+		Object json = this.getRequestBody(request);
 		
 		return null;
 	}
-	
-	
-	
-	
+
+	public Object getRequestBody(HttpServletRequest request) {
+		try {
+			Object json = request.getAttribute(REQUEST_BODY_ATTR);
+			if (json == null) {
+				json = IOUtils.toString(request.getInputStream(), ENCODING);
+				request.setAttribute(REQUEST_BODY_ATTR, json);
+				return json;
+			}
+			return json;
+		} catch (IOException e) {
+			throw new RuntimeException(e);
+		}
+	}
+
 	@Override
 	public boolean supportsParameter(MethodParameter parameter) {
 		return parameter.getParameterAnnotation(JsonArg.class) != null;
